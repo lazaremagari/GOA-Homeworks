2a60b97ba7bac4dcdb805e9412b8348815b7d380
lazaremagari <toreli1982@gmail.com>
2024-05-03 11:10:18 +0400

    commit

--- Day 24/homework/homework2.py
+++ Day 24/homework/homework2.py
@@ -1,2 +1,5 @@
-for i in range(int(input("enter number here -> "))):
+# მომხმარებელს შემოატანინეთ რამე რიცხვი და იმდენჯერ დაპრინტეთ 
+# "hello world", for loop ის გამოყენებით.
+num = int(input("enter number here -> "))
+for i in range(num):
     print("hello world")
