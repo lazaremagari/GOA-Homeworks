bc507dd03424dd727219cae10f5f09fd9611cd17
lazaremagari <toreli1982@gmail.com>
2024-05-11 22:03:17 +0400

    commit

---
+++ Day 28/classwork/classwork.py
@@ -0,0 +1,7 @@
+point = int(input("Enter Your Point -> "))
+if point > 90:
+    print("You can get your certificate")
+elif point > 70:
+    print("Upgrade it!")
+else:
+    print("it's so bad result")
---
+++ Day 28/homework/homework.py
@@ -0,0 +1,8 @@
+windows = input("enter best windows here -> ")
+if windows == "Windows 7":
+    print("no it's so old mouse")
+elif windows == "Windows 8":
+    print("no, Upgrade it!")
+elif windows == "Windows 10":
+    print("yeah that's best windows!")
+else("so bad!!")
