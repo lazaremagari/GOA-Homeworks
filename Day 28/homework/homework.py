93ba72bc84604e13fb94671447c1957a847f5ace
lazaremagari <toreli1982@gmail.com>
2024-05-12 12:17:12 +0400

    commit

--- Day 28/homework/homework.py
+++ Day 28/homework/homework.py
@@ -1,9 +1,11 @@
 windows = input("enter best windows here -> ")
 if windows == "Windows 7":
-    print("no it's so old mouse")
+    print("no it's so old windows")
 elif windows == "Windows 8":
     print("no, Upgrade it!")
 elif windows == "Windows 10":
     print("yeah that's best windows!")
+elif windows == "Windows 11":
+    print("it has got a few bugs!")
 else:
-    ("so bad!!")
+    print("so bad!!")
