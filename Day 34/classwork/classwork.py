529fc9eb453e6b9e651a9d3440e849eed768aa2d
lazaremagari <toreli1982@gmail.com>
2024-06-08 18:33:27 +0400

    commit

---
+++ Day 34/classwork/classwork.py
@@ -0,0 +1,11 @@
+group7_students = ["Mr. Gabriel Molodini(mentor)", "Andria", "alexandre", "mate", "lazare"]
+
+print(group7_students)
+
+group7_students[0] = "gabrieli"
+group7_students[1] = "demetre"
+group7_students[2] = "nikolozi"
+group7_students[3] = "merabi"
+group7_students[4] = "gio"
+
+print(group7_students)
---
+++ Day 34/homework/homework.py
@@ -0,0 +1,7 @@
+cars = ["nissan", "jeep", "range rover", "subaru"]
+cars[0] = "opel"
+cars[1] = "toyota"
+cars[2] = "mercedes"
+cars[3] = "ford"
+
+print(cars)
