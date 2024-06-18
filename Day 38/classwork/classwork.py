70c03c2d2069a6e03331d9ff83dbcf33689571ed
lazaremagari <toreli1982@gmail.com>
2024-06-18 13:28:08 +0400

    commit

---
+++ Day 38/classwork/classwork.py
@@ -0,0 +1,6 @@
+foods = ["fasanauruli xinkali", "mwvadi",
+          "kaxuri gvino", "lobio", "shotis puri", "gipi", "qartuli musikebi", 
+          "bigetnias qababi", "kompoti"]
+print(foods[0:3])
+print(foods[3:])
+print(foods[1:3])
