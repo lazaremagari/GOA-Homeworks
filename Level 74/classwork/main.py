d9483963a7689a4d1c9cfc31c0eb90e1145014d3
lazaremagari <toreli1982@gmail.com>
2024-10-24 20:08:12 +0400

    commit

---
+++ Level 74/classwork/main.py
@@ -0,0 +1,5 @@
+input1 = int(input("enter first number -> "))
+input2 = int(input("enter second number -> "))
+def argumentebi(a1, a2):
+    return a1 + a2
+print(argumentebi(input1, input2))
---
+++ Level 74/classwork/main2.py
@@ -0,0 +1,5 @@
+def list_index(num, sia):
+    l = sia[num]
+    return l * num
+
+print(list_index(6, [3443, 858, 278372, 9128938, 8732, 978293]))
